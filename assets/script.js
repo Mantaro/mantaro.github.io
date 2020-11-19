42fce6d7269c03e628bce4871ad39381bb69575c
Kodehawa <david.alejandro.rubio@gmail.com>
2020-11-18 23:05:11 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
 function applyTheme(theme) {
     document.body.classList.remove("theme-auto", "theme-light", "theme-dark");
     document.body.classList.add(`theme-${theme}`);
+    document.strong.classList.remove("theme-auto", "theme-light", "theme-dark");
+    document.strong.classList.add(`theme-${theme}`);
+    document.code.classList.remove("theme-auto", "theme-light", "theme-dark");
+    document.code.classList.add(`theme-${theme}`);
+    document.h2.classList.remove("theme-auto", "theme-light", "theme-dark");
+    document.h2.classList.add(`theme-${theme}`);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
